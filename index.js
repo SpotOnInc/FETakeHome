eda24ffac23a491952dc7b1a1489c06530bb42c1
Josh Klar <j@iv597.com>
2015-12-04 14:58:11 -0600

    NodeJS server to power this task

---
+++ index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const joi = require('joi');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const log = require('simple-node-logger').createSimpleLogger();
+
+const db = require('lowdb')('db.json', {
+    promise: true,
+})('users');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+// password must contain at least one number or special character
+// and be of mixed case
+const passwordRegex = /^(?=.*[0-9!@#\$%\^\&*\)\(+=._-])(?=.*[a-z])(?=.*[A-Z])(.*)$/;
+
+const registrationSchema = {
+    email: joi.string().required().email(),
+    password: joi.string().required().min(8).regex(passwordRegex),
+};
+
+app.get('/', (req, res) => {
+});
+
+app.post('/register', (req, res) => {
+    const validation = joi.validate(req.body, registrationSchema, {
+        abortEarly: false,
+        skipFunctions: true,
+        stripUnknown: true,
+    });
+
+    if (validation.error) {
+        return res.status(400).send(validation.error.details);
+    }
+
+    return db.find({
+        email: req.body.email,
+    }).then((user) => {
+        if (user) {
+            throw new Error(`'${ req.body.email }' is already a registered email address`);
+        }
+
+        return db.push({
+            email: req.body.email,
+            password: req.body.password, // obviously unsafe
+            created: Date.now(),
+        });
+    }).then((data) => {
+        log.info(`'${ data[0].email }' registered`);
+
+        res.set('Content-Type', 'application/json');
+
+        return res.send({
+            email: data[0].email,
+            created: data[0].created,
+        });
+    }).catch((err) => {
+        log.error(err.message);
+
+        const conflict = err.message.indexOf('already registered') >= 0;
+
+        return res.status(conflict ? 409 : 500).send({
+            message: err.message,
+        });
+    });
+});
+
+const server = app.listen(parseInt(process.env.PORT) || 5000, '0.0.0.0', () => {
+    log.info(`Server started on port ${ server.address().port }`);
+});
+
